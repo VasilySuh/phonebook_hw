8a264374d5b6bb1a2b50e874f7093cf53752ff1a
Vasily <vsuhorukov.vas@yandex.com>
2024-04-06 20:15:54 +0700

    The program works, but it is not stable

--- contact_book.py
+++ contact_book.py
@@ -1,60 +1,85 @@
-import sqlite3 as sl
+import sqlite3
 
-conn = sl.connect('contactbook.db')
+conn = sqlite3.connect('phonebook.db')
 cursor = conn.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY, name TEXT, phone_number TEXT)')
+# Создание таблицы
+cursor.execute('''CREATE TABLE IF NOT EXISTS phonebook
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                phone_number TEXT NOT NULL)''')
 
-conn.commit()
-conn.close()
-
-def show_contacts():
-   conn = sl.connect('contactbook.db')
-   cursor = conn.cursor()
-   
-   cursor.execute('SELECT * FROM contacts')
-   contacts = cursor.fetchall()
-   
-   for contact in contacts:
-      print(f'ID: {contact[0]}, Имя: {contact[1]}, Номер телефона: {contact[2]}')
-   
-   conn.close()
-
+# Добавление контакта
 def add_contact(name, phone_number):
-   conn = sl.connect('contactbook.db')
-   cursor = conn.cursor()
-   
-   cursor.execute('INSERT INTO contacts (name, phone_number) VALUES (?, ?)', (name, phone_number))
-   
-   conn.commit()
-   conn.close()
+    cursor.execute("INSERT INTO phonebook (name, phone_number) VALUES (?, ?)", (name, phone_number))
+    conn.commit()
+    print("Контакт успешно добавлен")
 
-def search_contacts(keyword):
-   conn = sl.connect('contactbook.db')
-   cursor = conn.cursor()
-   
-   cursor.execute('SELECT * FROM contacts WHERE name LIKE ? OR phone_number LIKE ?', ('%' + keyword + '%', '%' + keyword + '%'))
-   contacts = cursor.fetchall()
-   
-   for contact in contacts:
-       print(f'ID: {contact[0]}, Имя: {contact[1]}, Номер телефона: {contact[2]}')
-   
-   conn.close()
+# Просмотр всех контактов
+def view_contacts():
+    cursor.execute("SELECT * FROM phonebook")
+    contacts = cursor.fetchall()
+    for contact in contacts:
+        print(contact)
 
-def delete_contact(contact_id):
-   conn = sl.connect('contactbook.db')
-   cursor = conn.cursor()
-   
-   cursor.execute('DELETE FROM contacts WHERE id = ?', (contact_id,))
-   
-   conn.commit()
-   conn.close()
+# Поиск контакта по имени
+def search_contact(name):
+    cursor.execute("SELECT * FROM phonebook WHERE name=?", (name,))
+    contact = cursor.fetchone()
+    if contact:
+        print(contact)
+    else:
+        print("Контакт не найден")
 
-def update_contact(contact_id, name, phone_number):
-   conn = sqlite3.connect('contactbook.db')
-   cursor = conn.cursor()
-   
-   cursor.execute('UPDATE contacts SET name = ?, phone_number = ? WHERE id = ?', (name, phone_number, contact_id))
-   
-   conn.commit()
-   conn.close()
+# Удаление контакта
+def delete_contact(name):
+    cursor.execute("DELETE FROM phonebook WHERE name=?", (name,))
+    conn.commit()
+    print("Контакт успешно удален")
+
+# Изменение номера телефона
+def edit_contact(name, new_phone_number):
+    cursor.execute("UPDATE phonebook SET phone_number=? WHERE name=?", (new_phone_number, name))
+    conn.commit()
+    print("Номер телефона успешно изменен")
+
+while True:
+    print('Что вы хотите сделать?')
+    user_choice = input('\
+    1 - Посмотреть контакты\n\
+    2 - Найти контакт\n\
+    3 - Добавить контакт\n\
+    4 - Изменить контакт\n\
+    5 - Удалить контакт\n\
+    0 - Выйти из приложения\n')
+#     #6 - Просмотреть все контакты\n\
+    print()
+    if user_choice == '1':
+        print(view_contacts())
+        print()
+    elif user_choice == '2':
+        search_contact(name = input("Введите имя контакта: "))
+        print()
+    elif user_choice == '3':
+        add_contact(name = input("Введите имя: "))
+        print()
+        pass
+    elif user_choice == '4':
+        edit_contact(name = input("Введите имя контакта: "), phone_number = input("Введите номер телефона контакта: "))
+        print()
+        pass
+    elif user_choice == '5':
+        delete_contact(name = input("Введите имя контакта: "))
+        print()
+        pass
+#     # elif user_choice == '6':
+#     #     #show_phonebook(phonebook)
+#     #     pass
+    elif user_choice == '0':
+        print('До свидания!')
+        print()
+        break
+    else:
+        print('Неправильно выбрана команда!')
+        print()
+    continue
